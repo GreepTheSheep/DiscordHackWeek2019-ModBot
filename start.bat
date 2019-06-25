08754c07d8239a3c78353ad87fcbb31d0a44f159
Greep <89matt89.md@gmail.com>
2019-06-25 20:58:16 +0200

    Added if token file not exist

--- start.bat
+++ start.bat
@@ -1,12 +1,27 @@
 @echo off
+if not exist "token.json" (
+    echo Token file not found!
+    echo Please create a file named token.json
+    echo and type the following:
+    echo.
+    echo {"token": "YOUR_TOKEN"}
+    echo.
+    echo Please go to discordapp.com/developers to get your token.
+    echo.
+    pause
+    exit
+)
 if not exist "package-lock.json" (
+    echo Modules not installed.
+    echo Installing, it will take a while...
+    echo.
     npm install
     cls
     echo Starting now...
     echo.
     node bot.js
 ) else (
-    echo The bot is installed, starting...
+    echo Starting...
     echo.
     node bot.js
 )

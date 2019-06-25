53bf70f0ac3c2d70c7450d599a3ff3e18ee36fd5
Greep <89matt89.md@gmail.com>
2019-06-25 11:46:58 +0200

    up some syntax

--- bot.js
+++ bot.js
@@ -38,7 +38,8 @@ client.on('message', message => {
         .addField("Reason:", reason)
         .setTimestamp();
  
-        message.reply("Your report has been sent, thanks! :+1:");
+        message.delete();
+        message.reply("Your report has been sent, thanks! :+1:").then(msg => msg.delete(1000));
     
         client.guilds.get(message.guild.id).channels.get(message.guild.channels.find("name", logschannel).id).send(reportembed)
     }
@@ -48,7 +49,7 @@ client.on('message', message => {
         message.delete();
         if(!args[0]) return message.channel.send(`How many messages you want to clear ?\nExample: \`${prefix}clear 50\``).then(msg => msg.delete(3000));
         if (args[0] > 100) return message.channel.send(":negative_squared_cross_mark: You can not clear more than 100 messages!").then(msg => msg.delete(3000));
-        if (args[0] < 0) return message.channel.send(":negative_squared_cross_mark: You can not clear less than 1 messages!").then(msg => msg.delete(3000));
+        if (args[0] < 1) return message.channel.send(":negative_squared_cross_mark: You can not clear less than 1 messages!").then(msg => msg.delete(3000));
         message.channel.bulkDelete(args[0]).then(() => {
             message.channel.send(`:+1: **You have cleared ${args[0]} messages.**`).then(msg => msg.delete(3000));
         });
